f0148d73652533878f7722584f94c733002422c1
ChhayaPallavi <101050410+ChhayaPallavi@users.noreply.github.com>
2023-08-30 22:13:23 +0900

    commit

---
+++ src/main/java/in/ineuron/SpringbootMvcCrudappApplication.java
@@ -0,0 +1,13 @@
+package in.ineuron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMvcCrudappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMvcCrudappApplication.class, args);
+	}
+
+}
